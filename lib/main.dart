2fe1465cb9da6115e0a256ee82b125e889b03dc8
M Hamid Jamil <55445517+MHamidJamil420@users.noreply.github.com>
2022-09-25 20:09:51 +0500

    login_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 
@@ -14,12 +15,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/login",
+      // initialRoute: "/login",
       routes: {
-        "/": (context) => HomePage(),
-        "/login": (context) => loginPage(),
+        // "/": (context) => HomePage(),
+        // "/login": (context) => LoginPage(),
+        "/": (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,21 +1,59 @@
 import 'package:flutter/material.dart';
 
-class loginPage extends StatelessWidget {
+// ignore: use_key_in_widget_constructors
+class LoginPage extends StatelessWidget {
   // const loginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              "Login Page",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      // ignore: avoid_print
+                      print("Hi, Flutter");
+                    },
+                    child: const Text("Login"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
