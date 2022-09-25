8c6ed08e305be18f124743029c3ca16304f17d8b
M Hamid Jamil <55445517+MHamidJamil420@users.noreply.github.com>
2022-09-25 14:01:40 +0500

    day_2_comit

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int var_ = 420;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('This is my default text : $var_!'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
