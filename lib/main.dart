842597f37b0bfe3947b9b75b773a025d4b70092e
M Hamid Jamil <55445517+MHamidJamil420@users.noreply.github.com>
2022-09-27 02:23:09 +0500

    login_to_home_navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'pages/home_page.dart';
 import 'pages/login_page.dart';
+import 'utils/routes.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,9 +23,9 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       // initialRoute: "/login",
       routes: {
-        // "/": (context) => HomePage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
         // "/login": (context) => LoginPage(),
-        "/": (context) => LoginPage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,52 +8,56 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 20.0),
-            const Text(
-              "Login Page",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
               ),
-            ),
-            const SizedBox(height: 20.0),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 40.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Enter Username",
-                      labelText: "Username",
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  ElevatedButton(
-                    style: TextButton.styleFrom(),
-                    onPressed: () {
-                      // ignore: avoid_print
-                      print("Hi, Flutter");
-                    },
-                    child: const Text("Login"),
-                  ),
-                ],
+              const SizedBox(height: 20.0),
+              const Text(
+                "Login Page",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            )
-          ],
+              const SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 40.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    const SizedBox(height: 40.0),
+                    ElevatedButton(
+                      style: TextButton.styleFrom(
+                          minimumSize: const Size(120, 40)),
+                      onPressed: () {
+                        // ignore: avoid_print
+                        // print("Hi, Flutter");
+                        Navigator.pushNamed(context, "/home");
+                      },
+                      child: const Text("Login"),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
